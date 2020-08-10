70496e946b44e5c8f509b6080286b0f7e07f61d7
Xenia <lommy101@gmail.com>
2020-08-10 19:59:12 +0900

    fixed typing types..

--- main.py
+++ main.py
@@ -3,20 +3,21 @@ from youtube_api import YouTubeDataAPI
 
 class MyYoutube:
     def __init__(self, API_KEY: str):
-        self.Ayoutube  = YouTubeDataAPI(API_KEY)
+        self.Ayoutube: YouTubeDataAPI  = YouTubeDataAPI(API_KEY)
         self.ChID: str =  "PLoABXt5mipg4vxLw0NsRQLDDVBpOkshzF"
         
-    def getDataFrom_yt(self)->list:
-        searches = self.Ayoutube.get_videos_from_playlist_id(self.ChID)
+    def getDataFrom_yt(self)->List[str]:
+        searches: List[str] = self.Ayoutube.get_videos_from_playlist_id(self.ChID)
         return [x['video_id'] for x in searches]
 
-    def getMetaFrom_vd(self, Video_id_list: list)->list:
+    def getMetaFrom_vd(self, Video_id_list: List[str])->List[str]:
         return [self.Ayoutube.get_video_metadata(x)['video_description'] for x in Video_id_list]
 
 if __name__ == "__main__":
     API_KEY: str = ""
 
-    yt = MyYoutube(API_KEY)
+    yt: MyYoutube   = MyYoutube(API_KEY)
     
-    data: list = yt.getDataFrom_yt()
-    meta: list = yt.getMetaFrom_vd(data)
+    data: List[str] = yt.getDataFrom_yt()
+    meta: List[str] = yt.getMetaFrom_vd(data)
+    
